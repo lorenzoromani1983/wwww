a2916a2ad5713fc1c37636a516217502af74ce86
lorenzoromani1983 <24575445+lorenzoromani1983@users.noreply.github.com>
2021-05-31 14:16:01 +0200

    Update cluster_faces.py

--- wwww/cluster_faces.py
+++ wwww/cluster_faces.py
@@ -14,8 +14,8 @@ es = Elasticsearch('localhost:9200')
 G=networkx.DiGraph()
 data = es.search(index="face_recognition", size=1000000, body={"query": {"match_all": {}}}, request_timeout=1000)
 cwd = sys.path[0]
-output = cwd+"\\output"
-temp_dir = cwd+"\\temp"
+output = cwd+"/output"
+temp_dir = cwd+"/temp"
 
 arrays = []
 paths = []
